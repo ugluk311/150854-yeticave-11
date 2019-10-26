0e6603bfe7a77d89f95e81383b69dfcf19e5ad0b
Evgeniy Fadeev <ugluk311@gmail.com>
2019-10-26 11:54:50 +0300

    Edits a condition for showing username

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $user_name = 'Евгений';
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-            <?php if($is_auth == 1):?>
+            <?php if($is_auth === 1):?>
             <div class="user-menu__logged">
                 <p><?=$user_name?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
