a044af9062a09b9cdf363efba113bb856c5f151e
Evgeniy Fadeev <ugluk311@gmail.com>
2019-11-13 23:16:16 +0300

    Adds changes after validation

--- schema.sql
+++ schema.sql
@@ -1,50 +1,50 @@
+CREATE DATABASE jeticave
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+USE jeticave;
 
-  CREATE TABLE lots (
-  lot_id INT AUTO_INCREMENT PRIMARY KEY,
-  title CHAR,
-  description TEXT,
-  url_image CHAR,
-  first_price DECIMAL,
+CREATE TABLE categories(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category VARCHAR(64),
+  character_code VARCHAR(128) NOT NULL UNIQUE
+  );
+
+CREATE TABLE users(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_registration DATETIME DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(128) NOT NULL UNIQUE,
+  name VARCHAR(128) NOT NULL,
+  password VARCHAR(128) NOT NULL,
+  contact TEXT
+  );
+
+CREATE TABLE lots (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  url_image VARCHAR(200) NOT NULL,
+  first_price DECIMAL NOT NULL,
   date_finish TIMESTAMP,
-  rate_step DECIMAL,
-  user_id INT,
-  winner INT,
-  category_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (winner) REFERENCES users(user_id),
-  FOREIGN KEY (category_id) REFERENCES categories(category_id)
-
-  );
-  CREATE TABLE users(
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  date_registration DATE,
-  email CHAR NOT NULL UNIQUE,
-  name CHAR NOT NULL,
-  password CHAR NOT NULL,
-  contact CHAR,
-  lots_id INT,
-  rate_id INT,
-  FOREIGN KEY (lots_id) REFERENCES lots(lot_id),
-  FOREIGN KEY (rate_id) REFERENCES rates(rate_id)
+  bet_step DECIMAL,
+  user_id INT NOT NULL,
+  winner_id INT,
+  category_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (winner_id) REFERENCES users(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
   );
 
-  CREATE TABLE categories(
-  category_id INT AUTO_INCREMENT PRIMARY KEY,
-  category CHAR
-  );
-
-  CREATE TABLE rates(
-  rate_id INT AUTO_INCREMENT PRIMARY KEY,
-  date_post DATE,
+CREATE TABLE bets(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_post DATETIME DEFAULT CURRENT_TIMESTAMP,
   price DECIMAL NOT NULL,
   user_id INT,
   lot_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (lot_id) REFERENCES lots(lot_id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (lot_id) REFERENCES lots(id)
   );
 
-  CREATE UNIQUE INDEX i_email ON user(email);
-  CREATE INDEX i_name ON user(name);
-  CREATE INDEX title ON lot(title);
-  CREATE INDEX date_finish ON lot(date_finish);
-  CREATE INDEX date_post ON rate(date_post);
+CREATE UNIQUE INDEX i_email ON users(email);
+CREATE INDEX i_name ON users(name);
+CREATE INDEX date_finish ON lots(date_finish);
+CREATE INDEX date_post ON bets(date_post);
