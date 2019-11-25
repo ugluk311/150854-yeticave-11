4f0dcd3796e9f6db841d4e2761e8487c2f3d465a
Evgeniy Fadeev <ugluk311@gmail.com>
2019-11-25 22:05:51 +0300

    Adds lot-page

--- lot.php
+++ lot.php
@@ -8,39 +8,47 @@ if (!$con_db) {
     $error = mysqli_connect_error();
     $content = include_template('error.php', ['error' => $error]);
 } else {
-    $sql = <<<SQL
-SELECT l.id, title, first_price, url_image, date_finish, c.category,
-IFNULL((SELECT price FROM bets WHERE lot_id = l.id ORDER BY date_post DESC LIMIT 1), l.first_price) AS last_price
-FROM lots l
-JOIN categories c ON l.category_id = c.id
-WHERE date_finish > NOW()
-ORDER BY date_add DESC;
-SQL;
+    $sql = 'SELECT `id`, `category`, `character_code` FROM categories';
     $result = mysqli_query($con_db, $sql);
-
     if ($result) {
-        $product_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $product_category = mysqli_fetch_all($result, MYSQLI_ASSOC);
     } else {
         $error = mysqli_error($con_db);
         $content = include_template('error.php', ['error' => $error]);
     }
 }
 
-$sql = 'SELECT `id`, `category`, `character_code` FROM categories';
-$result = mysqli_query($con_db, $sql);
-if ($result) {
-    $product_category = mysqli_fetch_all($result, MYSQLI_ASSOC);
-} else {
-    $error = mysqli_error($con_db);
+
+if (isset($_GET['lot_id'])) {
+    $lot_id = filter_input(INPUT_GET, 'lot_id', FILTER_SANITIZE_NUMBER_INT);
+    $sql = <<<SQL
+SELECT l.id, title, description, first_price, url_image, date_finish, c.category
+FROM lots l
+JOIN categories c ON l.category_id = c.id
+WHERE  l.id = '$lot_id';
+SQL;
+    $result = mysqli_query($con_db, $sql);
+
+    if ($result) {
+        $lot_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        $error = mysqli_error($con_db);
+        $content = include_template('error.php', ['error' => $error]);
+    }
+    $page_content = include_template('lot.php', [
+        'product_category' => $product_category,
+        'lot_info' => $lot_info,
+    ]);
+} else{
+    http_response_code(404);
     $content = include_template('error.php', ['error' => $error]);
+    //$page_content = ;
 }
 
 
-$page_content = include_template('main.php', [
-    'product_category' => $product_category,
-    'product_info' => $product_info,
 
-]);
+
+
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
     'product_category' => $product_category,
---
+++ templates/_nav.php
@@ -0,0 +1,3 @@
+<li class="nav__item">
+    <a href="all-lots.html"><?= htmlspecialchars($item['category']) ?></a>
+</li>
